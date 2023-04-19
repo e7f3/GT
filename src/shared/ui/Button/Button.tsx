0decb4be844dce1654ca879a5f221a5df18a518b
Ilia <ilya@leadsforfinance.com>
2023-04-20 00:23:23 +0400

    feat: add Button ui component

---
+++ src/shared/ui/Button/Button.tsx
@@ -0,0 +1,46 @@
+import { ButtonHTMLAttributes, FC, ReactNode, memo } from 'react'
+
+import { classNames } from 'shared/lib/classNames/classNames'
+
+import classes from './Button.module.scss'
+
+export enum ButtonTheme {
+  'DEFAULT' = 'defaultTheme',
+  'INVERTED' = 'invertedTheme',
+}
+
+export enum ButtonColorScheme {
+  'DEFAULT' = 'defaultColorScheme',
+  'ALTERNATIVE' = 'alternativeColorScheme',
+}
+
+interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {
+  children?: ReactNode
+  className?: string
+  theme?: ButtonTheme
+  colorScheme?: ButtonColorScheme
+}
+
+export const Button: FC<Props> = memo((props) => {
+  const {
+    children,
+    className,
+    theme = ButtonTheme.DEFAULT,
+    colorScheme = ButtonColorScheme.DEFAULT,
+    ...restProps
+  } = props
+
+  return (
+    <button
+      className={classNames(classes.Button, {}, [
+        classes[theme],
+        classes[colorScheme],
+        className,
+      ])}
+      type='button'
+      {...restProps}
+    >
+      {children}
+    </button>
+  )
+})
